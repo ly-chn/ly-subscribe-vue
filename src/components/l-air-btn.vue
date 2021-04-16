fa26ca330cd5a8469a71cbaeba0ccc3a372f7a8a
3293232930@qq.com <xu984632>
2021-04-16 17:10:54 +0800

    l-air-btn.vue

---
+++ src/components/l-air-btn.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-btn class='bg-white shadow-none' color='primary' dark fab outlined x-small>
+    <v-icon @click.stop='$emit(`click`)'>{{ icon }}</v-icon>
+  </v-btn>
+</template>
+
+<script>
+export default {
+  name : "l-air-btn",
+  props: {
+    icon: {
+      type: String
+    },
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/components/task-tree.vue
+++ src/components/task-tree.vue
@@ -18,18 +18,18 @@
       </v-avatar>
     </template>
     <template v-slot:label="{item}">
-      <v-menu offset-y open-on-hover top>
+      <v-menu offset-y open-on-hover top transition="scale-transition">
         <template v-slot:activator="{ on, attrs }">
               <span v-bind="attrs" v-on="on">
                 {{ item.name }}
               </span>
         </template>
         <div class="operational-button select-none">
-          <v-icon @click.stop='onClick'>mdi-plus</v-icon>
+          <l-air-btn icon='mdi-plus' @click='onClick'/>
           &emsp;
-          <v-icon @click.stop='onClick'>mdi-plus</v-icon>
+          <l-air-btn icon='mdi-plus' @click='onClick'/>
           &emsp;
-          <v-icon @click.stop='onClick'>mdi-plus</v-icon>
+          <l-air-btn icon='mdi-plus' @click='onClick'/>
         </div>
       </v-menu>
     </template>
@@ -37,23 +37,26 @@
 </template>
 
 <script>
+import LAirBtn from "@/components/l-air-btn";
+
 export default {
-  name: "task-tree",
+  name      : "task-tree",
+  components: {LAirBtn},
   data() {
     return {
       // https://imgtu.com/i/ccvOmt
       // https://imgtu.com/i/ccvb6A
       // https://imgtu.com/i/ccvqOI
       items: [{
-        name: "项目一",
+        name   : "项目一",
         creator: {
           nickname: '张三',
-          avatar: 'https://z3.ax1x.com/2021/04/14/ccvOmt.jpg'
+          avatar  : 'https://z3.ax1x.com/2021/04/14/ccvOmt.jpg'
         }
       }, {
         name: "项目二",
       }, {
-        name: "项目三",
+        name    : "项目三",
         children: [{
           name: "UI",
         }, {
