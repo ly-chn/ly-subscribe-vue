82b538c60b50097fec8aa225ff78db7d9dba30c7
3293232930@qq.com <xu984632>
2021-04-19 22:19:50 +0800

    timeline

--- src/views/home/components/task-review.vue
+++ src/views/home/components/task-review.vue
@@ -1,6 +1,21 @@
 <template>
   <div class='w-full h-full'>
-    markdown remark
+    <v-btn color='green'>New Issues</v-btn>
+    <v-timeline align-top dense>
+      <v-timeline-item color="pink" small>
+        <v-row class="pt-1">
+          <v-col cols="3">
+            <strong>5pm</strong>
+          </v-col>
+          <v-col>
+            <strong>New Icon</strong>
+            <div class="caption">
+              Mobile App
+            </div>
+          </v-col>
+        </v-row>
+      </v-timeline-item>
+    </v-timeline>
   </div>
 </template>
 
